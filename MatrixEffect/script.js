0a3097096c40195086f9ed55bcaf066aefe8fa28
aaditiio <aaditinalawade19@gmail.com>
2026-02-21 20:11:05 +0530

    mouseenter matrix effect

---
+++ MatrixEffect/script.js
@@ -0,0 +1,24 @@
+const para = document.querySelector("p");
+const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+const text = para.innerText;
+let iteration = 0
+
+para.addEventListener("mouseenter", ()=>{
+    setInterval(randomText, 50)
+})
+
+function randomText(){
+    const str = text.split('').map((char, index)=>{
+
+        if(index < iteration){
+            return char
+        }
+        return characters.split('')[Math.floor(Math.random()*52)]
+    }).join('')
+
+    para.innerText = str
+
+    iteration += 0.25
+}
+
+    
